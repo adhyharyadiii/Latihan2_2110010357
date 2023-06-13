4b84708c8e8ec1df8c4e0308313f1f1ac870964b
adhyharyadiii <mhadhyhrd@gmail.com>
2023-06-13 08:49:24 +0800

    Menjalankan program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -3,6 +3,10 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        Mahasiswa adhy = new Mahasiswa("2110010357", "M. Adhy Haryadi", "TI");
         
+        System.out.println("NPM: "+adhy.npm);
+        System.out.println("Nama: "+adhy.nama);
+        System.out.println("Prodi: "+adhy.prodi);
     }
 }
